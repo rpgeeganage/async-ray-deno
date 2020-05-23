10550bb83173afa5baeb1ca66f930f34acd20257
rpgeeganage <rpgeeg@gmail.com>
2020-05-23 17:58:20 +0200

    fix file name

--- tests/every_test.ts
+++ tests/every_test.ts
@@ -1,42 +1,67 @@
 import { AsyncRay } from "../mod.ts";
 import { assertArrayContains, assertEquals } from "./test_deps.ts";
 
-const inputArray: number[] = [1, 2, 3, 4];
+const inputArray: number[] = [10, 20, 30, 40];
 
-async function dummy(condition: boolean): Promise<boolean> {
-  return Promise.resolve(condition);
+async function dummy(num: number, value: number): Promise<boolean> {
+  return Promise.resolve(num > value);
 }
 
-Deno.test("Find: should find", async function (): Promise<
+Deno.test("Every: should return a proper instance", async function (): Promise<
   void
 > {
-  const output = await AsyncRay(inputArray).aFind(
-    async (element: number, index?: number, collection?: number[]) => {
-      assertEquals(collection instanceof Array, true);
-      assertArrayContains(collection as number[], inputArray);
-      assertEquals(typeof element, "number");
-      assertEquals(typeof index, "number");
+  const output = await AsyncRay([]).aEvery(async () => true);
 
-      return dummy(element === 2);
-    },
-  );
-
-  assertEquals(output, 2);
+  assertEquals(typeof output, "boolean");
 });
 
-Deno.test("Find: should not find", async function (): Promise<
+Deno.test("Every: should return true if all the elements satisfy the condition", async function (): Promise<
   void
 > {
-  const output = await AsyncRay(inputArray).aFind(
+  const output = await AsyncRay(inputArray).aEvery(
     async (element: number, index?: number, collection?: number[]) => {
       assertEquals(collection instanceof Array, true);
       assertArrayContains(collection as number[], inputArray);
       assertEquals(typeof element, "number");
       assertEquals(typeof index, "number");
 
-      return dummy(element === 20);
+      return dummy(element, 5);
     },
   );
 
-  assertEquals(typeof output, "undefined");
+  assertEquals(output, true);
+});
+
+Deno.test("Every: should return false if at least one element did not satisfy the condition", async function (): Promise<
+  void
+> {
+  const output = await AsyncRay(inputArray).aEvery(
+    async (element: number, index?: number, collection?: number[]) => {
+      assertEquals(collection instanceof Array, true);
+      assertArrayContains(collection as number[], inputArray);
+      assertEquals(typeof element, "number");
+      assertEquals(typeof index, "number");
+
+      return dummy(element, 35);
+    },
+  );
+
+  assertEquals(output, false);
+});
+
+Deno.test("Every: should return false if none of elements satisfy the condition", async function (): Promise<
+  void
+> {
+  const output = await AsyncRay(inputArray).aEvery(
+    async (element: number, index?: number, collection?: number[]) => {
+      assertEquals(collection instanceof Array, true);
+      assertArrayContains(collection as number[], inputArray);
+      assertEquals(typeof element, "number");
+      assertEquals(typeof index, "number");
+
+      return dummy(element, 50);
+    },
+  );
+
+  assertEquals(output, false);
 });
--- tests/find_test.ts
+++ tests/find_test.ts
@@ -1,67 +1,42 @@
 import { AsyncRay } from "../mod.ts";
 import { assertArrayContains, assertEquals } from "./test_deps.ts";
 
-const inputArray: number[] = [10, 20, 30, 40];
+const inputArray: number[] = [1, 2, 3, 4];
 
-async function dummy(num: number, value: number): Promise<boolean> {
-  return Promise.resolve(num > value);
+async function dummy(condition: boolean): Promise<boolean> {
+  return Promise.resolve(condition);
 }
 
-Deno.test("Every: should return a proper instance", async function (): Promise<
+Deno.test("Find: should find", async function (): Promise<
   void
 > {
-  const output = await AsyncRay([]).aEvery(async () => true);
-
-  assertEquals(typeof output, "boolean");
-});
-
-Deno.test("Every: should return true if all the elements satisfy the condition", async function (): Promise<
-  void
-> {
-  const output = await AsyncRay(inputArray).aEvery(
+  const output = await AsyncRay(inputArray).aFind(
     async (element: number, index?: number, collection?: number[]) => {
       assertEquals(collection instanceof Array, true);
       assertArrayContains(collection as number[], inputArray);
       assertEquals(typeof element, "number");
       assertEquals(typeof index, "number");
 
-      return dummy(element, 5);
+      return dummy(element === 2);
     },
   );
 
-  assertEquals(output, true);
+  assertEquals(output, 2);
 });
 
-Deno.test("Every: should return false if at least one element did not satisfy the condition", async function (): Promise<
+Deno.test("Find: should not find", async function (): Promise<
   void
 > {
-  const output = await AsyncRay(inputArray).aEvery(
+  const output = await AsyncRay(inputArray).aFind(
     async (element: number, index?: number, collection?: number[]) => {
       assertEquals(collection instanceof Array, true);
       assertArrayContains(collection as number[], inputArray);
       assertEquals(typeof element, "number");
       assertEquals(typeof index, "number");
 
-      return dummy(element, 35);
+      return dummy(element === 20);
     },
   );
 
-  assertEquals(output, false);
-});
-
-Deno.test("Every: should return false if none of elements satisfy the condition", async function (): Promise<
-  void
-> {
-  const output = await AsyncRay(inputArray).aEvery(
-    async (element: number, index?: number, collection?: number[]) => {
-      assertEquals(collection instanceof Array, true);
-      assertArrayContains(collection as number[], inputArray);
-      assertEquals(typeof element, "number");
-      assertEquals(typeof index, "number");
-
-      return dummy(element, 50);
-    },
-  );
-
-  assertEquals(output, false);
+  assertEquals(typeof output, "undefined");
 });
